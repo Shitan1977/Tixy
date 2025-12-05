adb11753d7951ef2b04aee70f289a90d401fe246
Adele Iorio <adeleiorio.freestyle@gmail.com>
2025-12-06 00:41:27 +0100

    Aggiunto STATIC_ROOT per gestione staticfiles in produzione

--- core/core/settings.py
+++ core/core/settings.py
@@ -154,7 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# Percorso statici per produzione (server Aruba)
+STATIC_ROOT = "/var/www/vhosts/api.tixy.it/httpdocs/static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
